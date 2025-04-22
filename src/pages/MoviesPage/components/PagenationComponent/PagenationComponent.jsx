2767bfd3abb224211bce52816d8032f3ecbcfdb4
Sangmwi <gkstkdgnl12@naver.com>
2025-04-22 22:36:25 +0900

    123

--- src/pages/MoviesPage/components/PagenationComponent/PagenationComponent.jsx
+++ src/pages/MoviesPage/components/PagenationComponent/PagenationComponent.jsx
@@ -1,6 +1,38 @@
 import React from "react";
 import Pagination from "@mui/material/Pagination";
 
+
+const paginationStyle = {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 4,
+    paddingBottom: 4,
+    userSelect: "none",
+  
+    "& .MuiPaginationItem-root": {
+      color: "#ccc",
+      borderColor: "#555",
+      backgroundColor: "transparent",
+      transition: "all 0.2s ease-in-out",
+    },
+  
+    "& .MuiPaginationItem-root:hover": {
+      backgroundColor: "#333",
+    },
+  
+    "& .Mui-selected": {
+      backgroundColor: "var(--color-primary-darkest) !important",
+      color: "var(--color-secondary-light)",
+      fontWeight: "bold",
+      borderColor: "#aaa",
+    },
+  
+    "& .Mui-selected:hover": {
+      backgroundColor: "#eee",
+    },
+  };
+
+
 const PagenationComponent = ({ page, data, setSearchParams }) => {
   return (
     <Pagination
@@ -16,36 +48,7 @@ const PagenationComponent = ({ page, data, setSearchParams }) => {
         });
         window.scrollTo({ top: 0, behavior: "smooth" });
       }}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: 4,
-        paddingBottom: 4,
-
-
-        //스타일 지정
-        "& .MuiPaginationItem-root": {
-          color: "#ccc",
-          borderColor: "#555",
-          backgroundColor: "transparent",
-          transition: "all 0.2s ease-in-out",
-        },
-
-        "& .MuiPaginationItem-root:hover": {
-          backgroundColor: "#333",
-        },
-
-        "& .Mui-selected": {
-          backgroundColor: "var(--color-primary-darker)",
-          color: "var(--color-secondary)",
-          fontWeight: "bold",
-          borderColor: "#aaa",
-        },
-
-        "& .Mui-selected:hover": {
-          backgroundColor: "#eee",
-        },
-      }}
+        sx={paginationStyle}
     />
   );
 };
